289595eb8a25f07e39ab729d94f6aba280809231
ekaprog <115245831+ekaprog@users.noreply.github.com>
2023-02-13 16:46:29 +0300

    Update task_1.py

--- Лабораторная 3/task_1.py
+++ Лабораторная 3/task_1.py
@@ -10,18 +10,32 @@ class Book:
     def __repr__(self):
         return f"{self.__class__.__name__}(name={self._name!r}, author={self._author!r})"
 
+    @property
+    def name(self):
+        """Геттер """
+        return self._name
+
+    def author(self):
+        """Геттер"""
+        return self._author
+
 
 class PaperBook(Book):
     def __init__(self, name: str, author: str, pages: int):
         super().__init__(name, author)
-        if not isinstance(pages, int):
-            raise TypeError("Количество страниц должно быть типа int")
-        if pages <= 0:
-            raise ValueError("Количество страниц должно быть положительным числом")
         self.pages = pages
 
-    def __str__(self):
-        return f"Бумажная Книга {self._name}. Автор {self._author}. Количество страниц {self.pages}"
+    @property
+    def pages(self):
+        return self.pages
+
+    @pages.setter
+    def pages(self, new_pages: int) -> None:
+        if not isinstance(new_pages, int):
+            raise TypeError("Количество страниц должно быть типа int")
+        if new_pages <= 0:
+            raise ValueError("Количество страниц должно быть положительным числом")
+        self._pages = new_pages
 
     def __repr__(self) -> str:
         return f"{self.__class__.__name__}(name={self._name!r}, author={self._author!r}, pages={self.pages!r})"
@@ -30,14 +44,20 @@ class PaperBook(Book):
 class AudioBook(Book):
     def __init__(self, name: str, author: str, duration: float):
         super().__init__(name, author)
-        if not isinstance(duration, float):
-            raise TypeError("Длительность должна быть типа float")
-        if duration <= 0:
-            raise ValueError("Длительность должна быть положительным числом")
         self.duration = duration
 
-    def __str__(self):
-        return f"Аудиокнига {self._name}. Автор {self._author}. Длительность {self.duration}."
+    @property
+    def duration(self):
+        return self.duration
+
+    @duration.setter
+    def duration(self, duration_new) -> None:
+        if not isinstance(duration_new, float):
+            raise TypeError("Длительность должна быть типа float")
+        if duration_new <= 0:
+            raise ValueError("Длительность должна быть положительным числом")
+        self.duration = duration_new
 
     def __repr__(self) -> str:
         return f"{self.__class__.__name__}(name={self._name!r}, author={self._author!r}, duration={self.duration!r})"
+
